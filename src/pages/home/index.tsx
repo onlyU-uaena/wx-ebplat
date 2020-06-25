f5ac1f894a59318577a51762b31f84d9b751935a
kesiyuan <827936247@qq.com>
2020-06-25 16:12:19 +0800

    :arrow_up: update taro to 2.2.8

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import Taro, { useState } from "@tarojs/taro"
 import { View } from "@tarojs/components"
 import { AtButton } from 'taro-ui'
+import AccountVerification from '../../utils/accountVerification'
 
 interface Props {
 
@@ -8,8 +9,13 @@ interface Props {
 
 const Home: Taro.FC<Props> = () => {
 
+  const login = () => {
+    AccountVerification.loginWithPassword()
+  }
+
   return (
     <View>
+      <AtButton>test</AtButton>
     </View>
   )
 }
---
+++ src/utils/accountVerification/index.ts
@@ -0,0 +1,7 @@
+class AccountVerification {
+  static async loginWithPassword () {
+    console.log('need sub achieve')
+  }
+}
+
+export default AccountVerification
