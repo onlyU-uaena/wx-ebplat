430c832a2035325353a918d0e4600919fe574360
kesiyuan <827936247@qq.com>
2020-06-25 15:30:38 +0800

    :fire: remove unused code

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,9 @@
 import Taro, { useState } from "@tarojs/taro"
 import { View } from "@tarojs/components"
 import { AtTabBar } from 'taro-ui'
-import Mine from '../mine'
+import Home from '../home'
 
 interface Props {
-
 }
 
 const Index: Taro.FC<Props> = () => {
@@ -21,11 +20,11 @@ const Index: Taro.FC<Props> = () => {
   ]
   return (
     <View className='container'>
-      {currentTab === 0 && ''}
+      {currentTab === 0 && <Home />}
       {currentTab === 1 && ''}
       {currentTab === 2 && ''}
       {currentTab === 3 && ''}
-      {currentTab === 0 && <Mine />}
+      {currentTab === 4 && ''}
       <AtTabBar
         fixed
         fontSize={11}
